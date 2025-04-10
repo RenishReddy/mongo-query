85291ae9952d1e0b59d17abc6f73ae38cc01bf29
RenishReddy <alavalarenish@gmail.com>
2025-04-10 15:31:38 +0530

    first commit

---
+++ q1.js
@@ -0,0 +1,6 @@
+type mongosh in command prompt
+show dbs;
+use my db;
+show collections
+db.createCollection("products")
+show collections;
---
+++ q2.js
@@ -0,0 +1,6 @@
+db.products.insertOne(
+    {
+        name:"Jhon",
+        email:"jhon@email.com",
+        pass:"1234",
+    }); 
