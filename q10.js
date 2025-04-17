46a795313da53b01dd3a7ede0f3d4283e20b359d
RenishReddy <alavalarenish@gmail.com>
2025-04-17 14:51:06 +0530

    jjjj

--- q10.js
+++ q10.js
@@ -1,9 +1,18 @@
+db.employees.updateOne({ email: "mike@gmail.com" }, { $set: { salary: 1600 } });
+
+db.employees.updateOne({}, { $set: { org: "Wipro" } });
+db.employees.updateMany({}, { $set: { org: "Wipro" } });
+
 db.employees.updateOne(
-    {email:"mike@gmail.com"},
-    {$set: {org:"Wipro"}},
-    {upsert:true}
+  { email: "brian@gmail.com" },
+  { $set: { org: "Wipro" } },
+  { upsert: true }
 );
 
 db.employees.find(
-    {department: { $in:["HR","IT"]}}
+  { department: { $in: ["HR", "IT"] } }
+);
+
+db.employees.find(
+  { department: { $nin: ["HR", "IT"] } }
 );
