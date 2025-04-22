02edba73b32f194f03b97c99d38a7d8a4e2f64df
RenishReddy <alavalarenish@gmail.com>
2025-04-22 09:28:16 +0530

    nnn

--- q12.js
+++ q12.js
@@ -1,24 +1,60 @@
-db.employees.updateOne(
-    {email:"john@gmail.com"},
-    {$push:{score:4}}
+use mystore
+
+db.createCollection("Cars")
+
+db.Cars.insertMany([
+    {name:"m1"},
+    {name:"m2"},
+    {name:"m3"},
+    {name:"m4"},
+    {name:"m5"}]
 )
 
-db.employees.updateOne(
-    {email:"john@gmail.com"},
-    {$pull:{score:4}}
-)
-
-db.employees.updateMany(
+db.Cars.updateMany(
     {},
-    {$addToSet:{skills:"Python"}}
+    {$set:{price:10000}}
 )
 
-db.employees.updateMany(
-    {},
-    {$pop:{score:1}}
-)
+db.Cars.updateMany({},{$set:{variant:["v1","v2","v3","v4"]}})
 
-db.employees.updateMany(
-    {},
-    {$pop:{score:-1}}
+db.Cars.updateOne(
+    {name:'m1'},
+    {$set:{city:["Hyderabad"]}}
+
+)
+db.Cars.updateOne(
+    {name:'m2'},
+    {$set:{city:["Hyderabad"]}}
+
+)
+db.Cars.updateOne(
+    {name:'m3'},
+    {$set:{city:["Hyderabad"]}}
+
+)
+db.Cars.updateOne(
+    {name:'m2'},
+    {$set:{city:["Hyderabad"]}}
+
+)
+db.Cars.updateOne(
+    {name:'m4'},
+    {$set:{city:["Banglore"]}}
+
+)
+db.Cars.updateOne(
+    {name:'m4'},
+    {$set:{city:["Banglore"]}}
+
+)
+db.Cars.updateOne(
+    {name:'m5'},
+    {$set:{city:["Banglore"]}}
+
+)
+db.Cars.deleteMany(
+    {city:["Banglore"]}
+)
+db.Cars.findOne(
+    {}
 )
